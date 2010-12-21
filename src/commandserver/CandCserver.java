c11a7210c0a2aecce4d4525bb3a34260ecd8a8fb
Will Taff @pluto <wrtaff@gmail.com>
2010-12-21 10:49:22 -0800

    from pluto - made UI greatly more robust. Test 4.0.

--- src/commandserver/CandCserver.java
+++ src/commandserver/CandCserver.java
@@ -19,25 +19,30 @@ public class CandCserver {
 	 */
 	public static void main(String[] args) {
 		
-		//TODO CLEAN initialization stuff - have to make the database:
-		ClientDatabase testDBase = new ClientDatabase();
+		
+		///////////////////////////////////////////
+		//INITIALIZATION
+		///////////////////////////////////////////
+		
+		ClientDatabase dataBase = new ClientDatabase();
+		
+		Integer listenPort = 30000;
+
+		Socket clntSock = null;		
 				
-		//TODO - start the UI - will strike this
+		
+		//start the UI
 		Thread GUIthread = new Thread(
-				new CandCserverMenuUI(testDBase));
+				new CandCserverMenuUI(dataBase));
 		GUIthread.start();
 		
 
-		//we'll need to have a socket listener here
-		Integer port = 30000;
-
-		Socket clntSock = null;
-		System.out.println ("Server Listening on port " + port);
+		System.out.println ("Server Listening on port " + listenPort);
 		
 		
 		try {
 			
-			ServerSocket server = new ServerSocket (port);
+			ServerSocket server = new ServerSocket (listenPort);
 			
 			while (true){
 			
@@ -45,25 +50,23 @@ public class CandCserver {
 				
 				clntSock = server.accept();
 				
-				System.out.println ("Accepted from " 
+				System.out.println ("Connection Accepted from " 
 						+ clntSock.getInetAddress() );
 				
 				Thread thread = new Thread(
-						new ClientCommunicator(clntSock, testDBase));
+						new ClientCommunicator(clntSock, dataBase));
 				
 				thread.start();
 				
-				System.out.println("Created and started Thread: " + 
-						thread.getName() );
+				//System.out.println("Created and started Thread: " + 
+				//		thread.getName() );
 				
 				//shouldn't need to ever close the socket...
 				//server.close();
-				
-				//and we'll need to pass the cxn off here
-				System.out.println("sending socket off...");
 			
 			
 			}//end while
+			
 		}
 		catch (IOException ioe) {
 			System.err.println (ioe);
